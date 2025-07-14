401c82a9973a6ab205f1778079301ffefab1cde5
grngxd <36968271+grngxd@users.noreply.github.com>
2025-07-14 13:51:15 +0100

    idek man

---
+++ src/assets.ts
@@ -0,0 +1,9 @@
+// to find installed languages press ctrl+shift+p and type "change language mode"
+// https://github.com/leonardssh/vscord/blob/main/ADDING_LANGUAGE.md
+export const languages = [
+    "idle",
+
+    "log",
+    "plaintext",
+    "typescript",
+]
